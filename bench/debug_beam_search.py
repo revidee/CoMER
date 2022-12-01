fix: reduce mem usage during fixmatch training via gradient accumulation. Fix RandAug expecting PIL but numpy arrays were given. Add FixMatch train script to continue from a supervised checkpoint.

bench/debug_beam_search.py
@@ -1,16 +1,17 @@
-import time
-from typing import List, Tuple, Callable, Optional
+from typing import List
 from zipfile import ZipFile
 
-import numpy as np
 import torch
+import torchvision.transforms as tr
+from PIL.Image import Image
 from jsonargparse import CLI
+from torchvision.transforms import ToPILImage
 
-from comer.datamodules.crohme import extract_data_entries, DataEntry, vocab
+from comer.datamodules.crohme import extract_data_entries
 from comer.datamodules.crohme.batch import build_batches_from_samples, BatchTuple, Batch
-from comer.datamodules.crohme.variants.collate import collate_fn
-from comer.modules import CoMERSupervised
-from torch.profiler import profile, record_function, ProfilerActivity
+from comer.datamodules.crohme.dataset import W_LO, H_LO, H_HI, W_HI
+from comer.datamodules.utils.randaug import RandAugment
+from comer.datamodules.utils.transforms import ScaleToLimitRange
 
 checkpoint_path = "./bench/epoch3.ckpt"
 # checkpoint_path = "./bench/baseline_t112.ckpt"
@@ -26,30 +27,44 @@ def main(gpu: int = 1):
         device = torch.device(f"cuda:{gpu}")
 
     with torch.no_grad():
-        model = CoMERSupervised.load_from_checkpoint(checkpoint_path)
-        model = model.eval().to(device)
-        model.share_memory()
-
-        print("model loaded")
+        # model = CoMERSupervised.load_from_checkpoint(checkpoint_path)
+        # model = model.eval().to(device)
+        # model.share_memory()
+        #
+        # print("model loaded")
 
         with ZipFile("data.zip") as archive:
 
             batch_tuple: List[BatchTuple] = build_batches_from_samples(
-                extract_data_entries(archive, "2014", to_device=device),
+                extract_data_entries(archive, "2014"),
                 4,
                 batch_imagesize=(2200 * 250 * 4),
                 max_imagesize=(2200 * 250),
                 is_labled=True,
                 include_last_only_full=True
             )[::-1]
-            start_time = time.time()
-            for idx, batch_tup in enumerate(batch_tuple):
-                if idx > 0:
-                    break
-                batch: Batch = collate_fn([batch_tup]).to(device)
-                hyps = model.approximate_joint_search(batch.imgs, batch.mask, use_new=True, debug=False)
-                for i, hyp in enumerate(hyps):
-                    print(batch.img_bases[i], vocab.indices2words(hyp.seq))
+
+            trans_list = [ToPILImage(), RandAugment(3), ScaleToLimitRange(w_lo=W_LO, w_hi=W_HI, h_lo=H_LO, h_hi=H_HI)]
+            transform = tr.Compose(trans_list)
+
+            file_names, images, labels, is_labled, src_idx = batch_tuple[0]
+
+            transformed_ims = [transform(im) for im in images]
+
+            Image.show(ToPILImage()(transformed_ims[0]))
+            Image.show(ToPILImage()(transformed_ims[1]))
+            Image.show(ToPILImage()(transformed_ims[2]))
+            Image.show(ToPILImage()(transformed_ims[3]))
+
+
+            # start_time = time.time()
+            # for idx, batch_tup in enumerate(batch_tuple):
+            #     if idx > 0:
+            #         break
+            #     batch: Batch = collate_fn([batch_tup]).to(device)
+            #     hyps = model.approximate_joint_search(batch.imgs, batch.mask, use_new=True, debug=False)
+            #     for i, hyp in enumerate(hyps):
+            #         print(batch.img_bases[i], vocab.indices2words(hyp.seq))
                 # hyps = model.approximate_joint_search(batch.imgs, batch.mask, use_new=False)
                 # hyps_new = model.approximate_joint_search(batch.imgs, batch.mask, use_new=True)
                 # for i, hyp_old in enumerate(hyps):
@@ -68,7 +83,7 @@ def main(gpu: int = 1):
             # prof.export_stacks("profiler_stacks_cpu.txt", "self_cpu_time_total")
             # torch.save(prof.key_averages().table(sort_by="self_cpu_time_total"), "profiler_table.txt")
             # prof.export_chrome_trace("profiler_chrome.json")
-            print("total: ", time.time() - start_time)
+            # print("total: ", time.time() - start_time)
 
 
             # n = 2

comer/datamodules/crohme/dataset.py
@@ -2,6 +2,7 @@ from typing import List
 
 import torchvision.transforms as tr
 from torch.utils.data.dataset import Dataset
+from torchvision.transforms import ToPILImage
 
 from comer.datamodules.crohme import BatchTuple
 from comer.datamodules.utils.randaug import RandAugment
@@ -10,10 +11,10 @@ from comer.datamodules.utils.transforms import ScaleAugmentation, ScaleToLimitRa
 K_MIN = 0.7
 K_MAX = 1.4
 
-H_LO = 32
-H_HI = 512
-W_LO = 32
-W_HI = 2048
+H_LO = 16
+H_HI = 256
+W_LO = 16
+W_HI = 1024
 
 
 class CROHMEDataset(Dataset):
@@ -32,6 +33,7 @@ class CROHMEDataset(Dataset):
         if aug_mode == "weak":
             trans_list.append(ScaleAugmentation(K_MIN, K_MAX))
         elif aug_mode == "strong":
+            trans_list.append(ToPILImage())
             trans_list.append(RandAugment(3))
 
         trans_list += [

comer/datamodules/crohme/variants/fixmatch.py
@@ -1,16 +1,16 @@
-from typing import Optional, Any
+from typing import Optional, Any, List
 from zipfile import ZipFile
 
 from torch.utils.data.dataloader import DataLoader
 
 from comer.datamodules import CROHMESupvervisedDatamodule
 from comer.datamodules.crohme import build_dataset, extract_data_entries, get_splitted_indices, \
-    build_batches_from_samples, DataEntry
+    build_batches_from_samples, DataEntry, BatchTuple
 from comer.datamodules.crohme.dataset import CROHMEDataset
 from comer.datamodules.crohme.variants.collate import collate_fn, collate_fn_remove_unlabeled
 
 
-class CROHMESelfTrainingDatamodule(CROHMESupvervisedDatamodule):
+class CROHMEFixMatchDatamodule(CROHMESupvervisedDatamodule):
     def setup(self, stage: Optional[str] = None) -> None:
         with ZipFile(self.zipfile_path) as archive:
             if stage == "fit" or stage is None:
@@ -47,7 +47,7 @@ class CROHMESelfTrainingDatamodule(CROHMESupvervisedDatamodule):
                 # train_batch_size, since the losses for both are averaged together
                 # Like FixMatch, we increase the batch-size by the factor
                 unlabeled_factor = (1 / (1 - self.unlabeled_pct)) - 1
-                unlabeled_batch_size = self.train_batch_size * unlabeled_factor
+                unlabeled_batch_size = int(self.train_batch_size * unlabeled_factor)
                 if unlabeled_batch_size < 1:
                     unlabeled_batch_size = 1
 
@@ -74,7 +74,7 @@ class CROHMESelfTrainingDatamodule(CROHMESupvervisedDatamodule):
                     "",
                 )
 
-    def add_labels_to_batches(self, batches: list[tuple[list[str], list[ndarray], list[list[str]], bool, int]]):
+    def add_labels_to_batches(self, batches: List[BatchTuple]):
         if self.trainer is None or self.trainer.unlabeled_pseudo_labels is None:
             return batches
         for idx, src_batch in enumerate(batches):
@@ -85,19 +85,40 @@ class CROHMESelfTrainingDatamodule(CROHMESupvervisedDatamodule):
         return batches
 
     def get_unlabeled_for_train(self):
-        return self.add_labels_to_batches(self.pseudo_labeled_batches)
+        self.add_labels_to_batches(self.pseudo_labeled_batches)
+        filtered_batches = []
+        total_labeled = 0
+        for idx, src_batch in enumerate(self.pseudo_labeled_batches):
+            src_batch[2].clear()
+            src_batch[2].extend(
+                [self.trainer.unlabeled_pseudo_labels[fname] for fname in src_batch[0]]
+            )
+            for single_item_label in src_batch[2]:
+                if len(single_item_label) > 0:
+                    total_labeled += 1
+            for single_item_label in src_batch[2]:
+                if len(single_item_label) > 0:
+                    filtered_batches.append(src_batch)
+                    break
+        return filtered_batches
 
     def get_unlabeled_for_pseudo_labeling(self):
         return self.add_labels_to_batches(self.pseudo_labeling_batches)
 
     def train_dataloader(self):
+        unlabeled_with_pseudos = self.get_unlabeled_for_train()
+        labeled = DataLoader(
+            self.train_labeled_dataset,
+            shuffle=True,
+            num_workers=self.num_workers,
+            collate_fn=collate_fn,
+        )
+        if len(unlabeled_with_pseudos) == 0:
+            return {
+                "labeled": labeled
+            }
         return {
-            "labeled": DataLoader(
-                self.train_labeled_dataset,
-                shuffle=True,
-                num_workers=self.num_workers,
-                collate_fn=collate_fn,
-            ),
+            "labeled": labeled,
             "unlabeled": DataLoader(
                 CROHMEDataset(
                     self.get_unlabeled_for_train(),

comer/modules/fixmatch.py
@@ -1,9 +1,13 @@
-from typing import Dict
+from typing import Dict, Callable, List, Union, Tuple, Iterable
 
-from comer.datamodules.crohme import Batch
+import torch
+from pytorch_lightning.utilities.fetching import AbstractDataFetcher, DataLoaderIterDataFetcher
+
+from comer.datamodules.crohme import Batch, vocab
 from comer.modules import CoMERSelfTraining
 from comer.utils.utils import (ce_loss,
                                to_bi_tgt_out)
+import torch.distributed as dist
 
 
 class CoMERFixMatch(CoMERSelfTraining):
@@ -21,20 +25,86 @@ class CoMERFixMatch(CoMERSelfTraining):
         out_hat = self(batches["labeled"].imgs, batches["labeled"].mask, tgt)
 
         loss = ce_loss(out_hat, out)
+        loggable_total_loss = float(loss)
 
-        batch_size = batches["labeled"].imgs.shape[0]
+        batch_size = len(batches["labeled"])
+        train_batch_size = batch_size
 
         if "unlabeled" in batches:
-            tgt, out = to_bi_tgt_out(batches["unlabeled"].labels, self.device)
-            out_hat = self(batches["unlabeled"].imgs, batches["unlabeled"].mask, tgt)
-
+            unlabeled_size = len(batches["unlabeled"])
             # the ce_loss is reduced by mean with the amount of pseudo-labeled samples, which passed the threshold.
             # To align with FixMatch, this has to be normalized to all samples, including below-threshold samples.
-            loss += ce_loss(out_hat, out) * self.hparams.lambda_u * (
-                    len(batches["unlabeled"]) / batches["unlabeled"].unfiltered_size
+            norm_fac = (
+                    unlabeled_size / batches["unlabeled"].unfiltered_size
             )
+            # floor division, if we don't divide cleanly, we will miss some
+            steps = int(unlabeled_size // train_batch_size)
+            # if there is any remained, add an extra step
+            if (unlabeled_size % train_batch_size) != 0:
+                steps += 1
+            for i in range(steps):
+                loss.backward()
+                start, end = i * train_batch_size, (i + 1) * train_batch_size
+                if end > unlabeled_size:
+                    end = unlabeled_size
+                labels, imgs, mask = batches["unlabeled"].labels[start:end],\
+                    batches["unlabeled"].imgs[start:end],\
+                    batches["unlabeled"].mask[start:end]
+                tgt, out = to_bi_tgt_out(labels, self.device)
+                out_hat = self(imgs, mask, tgt)
+                # average with the amount of samples we processed in this iteration vs. total samples
+                # e.g. if we have 25 total, and each iter has 5, this will be weighted with 1/5 (5/25)
+                loss = ce_loss(out_hat, out) * ((end - start) / unlabeled_size) * norm_fac * self.hparams.lambda_u
+                loggable_total_loss += float(loss)
             batch_size += batches["unlabeled"].imgs.shape[0]
 
-        self.log("train_loss", loss, on_step=False, on_epoch=True, sync_dist=True, batch_size=batch_size)
+        self.log("train_loss", loggable_total_loss, on_step=False, on_epoch=True, sync_dist=True, batch_size=batch_size)
         return loss
+
+    def unlabeled_full(self, data_fetcher: AbstractDataFetcher,
+                       start_batch: Callable, end_batch: Callable, dataloader_idx: int):
+        is_iter_data_fetcher = isinstance(data_fetcher, DataLoaderIterDataFetcher)
+        fnames: List[str] = []
+        pseudo_labels: List[List[str]] = []
+        batch: Batch
+
+        batch_idx = 0
+
+        with torch.inference_mode():
+            while not data_fetcher.done:
+                if not is_iter_data_fetcher:
+                    batch = next(data_fetcher)
+                else:
+                    _, batch = next(data_fetcher)
+                start_batch(batch, batch_idx)
+                batch_idx = batch_idx + 1
+
+                fnames.extend(batch.img_bases)
+                # Why h.score / 2?
+                # h.score is re-weighted by a final scoring run,
+                # adding the loss of a reversed target sequence to the normalized log-likelihood of the beam search.
+                # By dividing with 2, we average between these to get a kind-of log-likelihood again.
+                pseudo_labels.extend(
+                    [
+                        vocab.indices2words(h.seq) if (h.score / 2) >= self.pseudo_labeling_threshold
+                        else [] for h in self.approximate_joint_search(batch.imgs, batch.mask)]
+                )
+
+                end_batch(batch, batch_idx)
+        return zip(fnames, pseudo_labels)
+
+    def validation_unlabeled_step_end(self, to_gather: Iterable[Tuple[int, List[List[str]]]]):
+        if not hasattr(self.trainer, 'unlabeled_pseudo_labels'):
+            print("warn: trainer does not have the pseudo-label state, cannot update pseudo-labels")
+            return
+
+        all_gpu_labels: List[Union[None, List[Tuple[str, List[str]]]]] = [None for _ in range(dist.get_world_size())]
+        dist.barrier()
+        dist.all_gather_object(all_gpu_labels, list(to_gather))
+        # update the gpu-local trainer-cache
+        for single_gpu_labels in all_gpu_labels:
+            if single_gpu_labels is None:
+                continue
+            for fname, label in single_gpu_labels:
+                self.trainer.unlabeled_pseudo_labels[fname] = label
 