fc71007188b3151ca8c8c161822af10bb2b2e37a
revidee <30733726+revidee@users.noreply.github.com>
2022-12-08 18:07:49 +0100

    fix: mask batchnorm in the encoder, s.t. padding doesn't impact training convergence

--- comer/model/encoder.py
+++ comer/model/encoder.py
@@ -1,24 +1,143 @@
 import math
-from typing import Tuple
+from typing import Tuple, Optional
 
 import pytorch_lightning as pl
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from einops.einops import rearrange
-from torch import FloatTensor, LongTensor
+from einops.einops import rearrange, repeat
+from torch import FloatTensor, LongTensor, Tensor
+from torch.nn.modules.batchnorm import _BatchNorm
 
 from .pos_enc import ImgPosEnc
 
+
+# Masked Batch Normalization
+
+def masked_batch_norm(input: Tensor, mask: Tensor, weight: Optional[Tensor], bias: Optional[Tensor],
+                      running_mean: Optional[Tensor], running_var: Optional[Tensor], training: bool,
+                      momentum: float, eps: float = 1e-5) -> Tensor:
+    r"""Applies Masked Batch Normalization for each channel in each data sample in a batch.
+    See :class:`~MaskedBatchNorm1d`, :class:`~MaskedBatchNorm2d`, :class:`~MaskedBatchNorm3d` for details.
+    """
+    if not training and (running_mean is None or running_var is None):
+        raise ValueError('Expected running_mean and running_var to be not None when training=False')
+
+    num_dims = len(input.shape[2:])
+    _dims = (0,) + tuple(range(-num_dims, 0))
+    _slice = (None, ...) + (None,) * num_dims
+
+    if training:
+        num_elements = mask.sum(_dims)
+        mean = (input * mask).sum(_dims) / num_elements  # (C,)
+        var = (((input - mean[_slice]) * mask) ** 2).sum(_dims) / num_elements  # (C,)
+
+        if running_mean is not None:
+            running_mean.copy_(running_mean * (1 - momentum) + momentum * mean.detach())
+        if running_var is not None:
+            running_var.copy_(running_var * (1 - momentum) + momentum * var.detach())
+    else:
+        mean, var = running_mean, running_var
+
+    out = (input - mean[_slice]) / torch.sqrt(var[_slice] + eps)  # (N, C, ...)
+
+    if weight is not None and bias is not None:
+        out = out * weight[_slice] + bias[_slice]
+
+    return out
+
+class _MaskedBatchNorm(_BatchNorm):
+    def __init__(self, num_features, eps=1e-5, momentum=0.1, affine=True,
+                 track_running_stats=True):
+        super(_MaskedBatchNorm, self).__init__(
+            num_features, eps, momentum, affine, track_running_stats)
+
+    def forward(self, input: Tensor, mask: Tensor = None) -> Tensor:
+        self._check_input_dim(input)
+        if mask is not None:
+            self._check_input_dim(mask)
+
+        if self.momentum is None:
+            exponential_average_factor = 0.0
+        else:
+            exponential_average_factor = self.momentum
+
+        if self.training and self.track_running_stats:
+            if self.num_batches_tracked is not None:
+                self.num_batches_tracked = self.num_batches_tracked + 1
+                if self.momentum is None:  # use cumulative moving average
+                    exponential_average_factor = 1.0 / float(self.num_batches_tracked)
+                else:  # use exponential moving average
+                    exponential_average_factor = self.momentum
+
+        r"""
+        Decide whether the mini-batch stats should be used for normalization rather than the buffers.
+        Mini-batch stats are used in training mode, and in eval mode when buffers are None.
+        """
+        if self.training:
+            bn_training = True
+        else:
+            bn_training = (self.running_mean is None) and (self.running_var is None)
+
+        r"""
+        Buffers are only updated if they are to be tracked and we are in training mode. Thus they only need to be
+        passed when the update should occur (i.e. in training mode when they are tracked), or when buffer stats are
+        used for normalization (i.e. in eval mode when buffers are not None).
+        """
+        if mask is None:
+            return F.batch_norm(
+                input,
+                # If buffers are not to be tracked, ensure that they won't be updated
+                self.running_mean if not self.training or self.track_running_stats else None,
+                self.running_var if not self.training or self.track_running_stats else None,
+                self.weight, self.bias, bn_training, exponential_average_factor, self.eps
+            )
+        else:
+            return masked_batch_norm(
+                input, mask, self.weight, self.bias,
+                self.running_mean if not self.training or self.track_running_stats else None,
+                self.running_var if not self.training or self.track_running_stats else None,
+                bn_training, exponential_average_factor, self.eps
+            )
+
+class MaskedBatchNorm1d(torch.nn.BatchNorm1d, _MaskedBatchNorm):
+    r"""Applies Batch Normalization over a masked 3D input
+    (a mini-batch of 1D inputs with additional channel dimension)..
+    See documentation of :class:`~torch.nn.BatchNorm1d` for details.
+    Shape:
+        - Input: :math:`(N, C, L)`
+        - Mask: :math:`(N, 1, L)`
+        - Output: :math:`(N, C, L)` (same shape as input)
+    """
+
+    def __init__(self, num_features: int, eps: float = 1e-5, momentum: float = 0.1,
+                 affine: bool = True, track_running_stats: bool = True) -> None:
+        super(MaskedBatchNorm1d, self).__init__(
+            num_features, eps, momentum, affine, track_running_stats)
+
+class MaskedBatchNorm2d(torch.nn.BatchNorm2d, _MaskedBatchNorm):
+    r"""Applies Batch Normalization over a masked 4D input
+    (a mini-batch of 2D inputs with additional channel dimension)..
+    See documentation of :class:`~torch.nn.BatchNorm2d` for details.
+    Shape:
+        - Input: :math:`(N, C, H, W)`
+        - Mask: :math:`(N, 1, H, W)`
+        - Output: :math:`(N, C, H, W)` (same shape as input)
+    """
+
+    def __init__(self, num_features: int, eps: float = 1e-5, momentum: float = 0.1,
+                 affine: bool = True, track_running_stats: bool = True) -> None:
+        super(MaskedBatchNorm2d, self).__init__(
+            num_features, eps, momentum, affine, track_running_stats)
 
 # DenseNet-B
 class _Bottleneck(nn.Module):
     def __init__(self, n_channels: int, growth_rate: int, use_dropout: bool):
         super(_Bottleneck, self).__init__()
         interChannels = 4 * growth_rate
-        self.bn1 = nn.BatchNorm2d(interChannels)
+        self.bn1 = MaskedBatchNorm2d(interChannels)
         self.conv1 = nn.Conv2d(n_channels, interChannels, kernel_size=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(growth_rate)
+        self.bn2 = MaskedBatchNorm2d(growth_rate)
         self.conv2 = nn.Conv2d(
             interChannels, growth_rate, kernel_size=3, padding=1, bias=False
         )
@@ -26,14 +145,15 @@ class _Bottleneck(nn.Module):
         self.dropout = nn.Dropout(p=0.2)
 
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)), inplace=True)
+        out = F.relu(self.bn1(self.conv1(x[0]), x[1]), inplace=True)
         if self.use_dropout:
             out = self.dropout(out)
-        out = F.relu(self.bn2(self.conv2(out)), inplace=True)
+        out = F.relu(self.bn2(self.conv2(out), x[1]), inplace=True)
         if self.use_dropout:
             out = self.dropout(out)
-        out = torch.cat((x, out), 1)
-        return out
+
+        out = torch.cat((x[0], out), 1)
+        return out, x[1]
 
 
 # single layer
@@ -48,24 +168,24 @@ class _SingleLayer(nn.Module):
         self.dropout = nn.Dropout(p=0.2)
 
     def forward(self, x):
-        out = self.conv1(F.relu(x, inplace=True))
+        out = self.conv1(F.relu(x[0], inplace=True))
         if self.use_dropout:
             out = self.dropout(out)
-        out = torch.cat((x, out), 1)
-        return out
+        out = torch.cat((x[0], out), 1)
+        return out, x[1]
 
 
 # transition layer
 class _Transition(nn.Module):
     def __init__(self, n_channels: int, n_out_channels: int, use_dropout: bool):
         super(_Transition, self).__init__()
-        self.bn1 = nn.BatchNorm2d(n_out_channels)
+        self.bn1 = MaskedBatchNorm2d(n_out_channels)
         self.conv1 = nn.Conv2d(n_channels, n_out_channels, kernel_size=1, bias=False)
         self.use_dropout = use_dropout
         self.dropout = nn.Dropout(p=0.2)
 
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)), inplace=True)
+        out = F.relu(self.bn1(self.conv1(x[0]), x[1]), inplace=True)
         if self.use_dropout:
             out = self.dropout(out)
         out = F.avg_pool2d(out, 2, ceil_mode=True)
@@ -87,7 +207,7 @@ class DenseNet(nn.Module):
         self.conv1 = nn.Conv2d(
             1, n_channels, kernel_size=7, padding=3, stride=2, bias=False
         )
-        self.norm1 = nn.BatchNorm2d(n_channels)
+        self.norm1 = MaskedBatchNorm2d(n_channels)
         self.dense1 = self._make_dense(
             n_channels, growth_rate, n_dense_blocks, bottleneck, use_dropout
         )
@@ -109,7 +229,7 @@ class DenseNet(nn.Module):
         )
 
         self.out_channels = n_channels + n_dense_blocks * growth_rate
-        self.post_norm = nn.BatchNorm2d(self.out_channels)
+        self.post_norm = MaskedBatchNorm2d(self.out_channels)
 
     @staticmethod
     def _make_dense(n_channels, growth_rate, n_dense_blocks, bottleneck, use_dropout):
@@ -124,19 +244,21 @@ class DenseNet(nn.Module):
 
     def forward(self, x, x_mask):
         out = self.conv1(x)
-        out = self.norm1(out)
-        out_mask = x_mask[:, 0::2, 0::2]
+        out_mask = repeat(~x_mask, "b h w -> b () h w")
+        out_mask = out_mask[:, :, ::2, ::2]
+        out = self.norm1(out, out_mask)
         out = F.relu(out, inplace=True)
         out = F.max_pool2d(out, 2, ceil_mode=True)
-        out_mask = out_mask[:, 0::2, 0::2]
-        out = self.dense1(out)
-        out = self.trans1(out)
-        out_mask = out_mask[:, 0::2, 0::2]
-        out = self.dense2(out)
-        out = self.trans2(out)
-        out_mask = out_mask[:, 0::2, 0::2]
-        out = self.dense3(out)
-        out = self.post_norm(out)
+        out_mask = out_mask[:, :, ::2, ::2]
+        out, _ = self.dense1((out, out_mask))
+        out = self.trans1((out, out_mask))
+        out_mask = out_mask[:, :, ::2, ::2]
+        out, _ = self.dense2((out, out_mask))
+        out = self.trans2((out, out_mask))
+        out_mask = out_mask[:, :, ::2, ::2]
+        out, _ = self.dense3((out, out_mask))
+        out = self.post_norm(out, out_mask)
+        out_mask = rearrange(~out_mask, "b () h w -> b h w")
         return out, out_mask
 
 
